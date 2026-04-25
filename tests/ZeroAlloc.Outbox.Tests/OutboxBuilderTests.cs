0fb372733edd35ada1f886a11f957f6a84f324b1
Marcel Roozekrans <marcel@roozekrans.nl>
2026-04-25 21:29:12 +0200

    feat(outbox.efcore)!: rename AddOutboxEfCore<TContext> to WithEfCore<TContext> on IOutboxBuilder
    BREAKING CHANGE: AddOutboxEfCore<TContext>(IServiceCollection) is retained
    as an [Obsolete] shim with diagnostic ID ZAOBOX003. New canonical form is
    services.AddOutbox().WithEfCore<TContext>().

--- tests/ZeroAlloc.Outbox.Tests/OutboxBuilderTests.cs
+++ tests/ZeroAlloc.Outbox.Tests/OutboxBuilderTests.cs
@@ -1,6 +1,8 @@
 using FluentAssertions;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.DependencyInjection;
 using ZeroAlloc.Outbox;
+using ZeroAlloc.Outbox.EfCore;
 using ZeroAlloc.Outbox.InMemory;
 
 namespace ZeroAlloc.Outbox.Tests;
@@ -43,5 +45,35 @@ public class OutboxBuilderTests
 #pragma warning restore CS0618
 
         services.BuildServiceProvider().GetService<IOutboxStore>().Should().NotBeNull();
+    }
+
+    [Fact]
+    public void WithEfCore_RegistersEfCoreStore()
+    {
+        var services = new ServiceCollection();
+        services.AddLogging();
+        services.AddDbContext<DashboardTestDbContext>(opts => opts.UseSqlite("DataSource=:memory:"));
+
+        services.AddOutbox().WithEfCore<DashboardTestDbContext>();
+
+        using var sp = services.BuildServiceProvider();
+        using var scope = sp.CreateScope();
+        scope.ServiceProvider.GetService<IOutboxStore>().Should().NotBeNull();
+    }
+
+    [Fact]
+    public void AddOutboxEfCore_LegacyShim_StillRegistersStore()
+    {
+        var services = new ServiceCollection();
+        services.AddLogging();
+        services.AddDbContext<DashboardTestDbContext>(opts => opts.UseSqlite("DataSource=:memory:"));
+
+#pragma warning disable CS0618 // exercise the legacy shim
+        services.AddOutboxEfCore<DashboardTestDbContext>();
+#pragma warning restore CS0618
+
+        using var sp = services.BuildServiceProvider();
+        using var scope = sp.CreateScope();
+        scope.ServiceProvider.GetService<IOutboxStore>().Should().NotBeNull();
     }
 }
